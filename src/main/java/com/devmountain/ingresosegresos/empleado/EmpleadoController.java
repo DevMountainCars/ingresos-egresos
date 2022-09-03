da0dcc213f248562e68130f1a12f73fe272d1c07
Marcela Aristizabal <marcea@Marcelas-MacBook-Pro.local>
2022-09-03 08:15:56 -0500

    feature/crearServicioEmpresa

--- src/main/java/com/devmountain/ingresosegresos/empleado/EmpleadoController.java
+++ src/main/java/com/devmountain/ingresosegresos/empleado/EmpleadoController.java
@@ -3,5 +3,5 @@ package com.devmountain.ingresosegresos.empleado;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class EmpleadoCotroller {
+public class EmpleadoController {
 }
